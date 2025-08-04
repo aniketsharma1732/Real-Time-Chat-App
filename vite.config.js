55237e04df3888a2f16f58ef164728aeaaed4fa5
Aniket Sharma <anikets.5061@gmail.com>
2025-08-04 18:28:31 +0530

    Updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Real-Time-Chat-App/", // 🔥 IMPORTANT for GitHub Pages
+  base: '/Real-Time-Chat-App/', // 👈 must match your GitHub repo name exactly
   plugins: [react()],
 });

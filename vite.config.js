def89cd9415ac83f52fd2d743fbc22bd7460ca7a
Aniket Sharma <anikets.5061@gmail.com>
2025-08-04 18:21:20 +0530

    Update vite.config.js with base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/real-time-chat-application/", // 🔥 IMPORTANT for GitHub Pages
+  base: "/Real-Time-Chat-App/", // 🔥 IMPORTANT for GitHub Pages
   plugins: [react()],
 });
